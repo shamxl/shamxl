b2e01e7f3552d36cc34fa2ba734a327f6e5f1ffe
shaxml <shaxml@pm.me>
2024-01-21 16:28:50 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("this piece of shit works!") // this line make sure that the code in the below actually works
+// this a variable
+var githubUsername = "shamxl" // im a good developer
+			// this a blank space
+			// this too
+			// and this
+var element = document.getElementById("hidden-link") // this is a variable
+element.addEventListener("click", () => {  // this line redirects you to my github
+	document.location.href = `https://github.com/${githubUsername}` // this is the thing that really redirects	
+}) // the end. tnaks <3
